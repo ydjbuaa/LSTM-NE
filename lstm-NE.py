4c3270888144c827fa10c04e48f8addbe4f51aaa
ydj <ydjbuaa@163.com>
2016-11-01 11:10:57 +0800

    add scripts to generate nodes-to-edges corpus

--- lstm-NE.py
+++ lstm-NE.py
@@ -2,7 +2,8 @@
 import numpy
 import theano.tensor
 from collections import OrderedDict
-
+import cPickle
+import time
 from lstm import *
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
@@ -12,6 +13,7 @@ SEED = 123
 numpy.random.seed(SEED)
 
 def load_embeddings(n_nodes, n_edges, dim=128):
+
     randn = numpy.random.rand(n_nodes, dim)
     node_embs = (randn * 0.01).astype(config.floatX)
 
@@ -27,7 +29,7 @@ def build_model(options):
     use_noise = theano.shared(numpy_floatX(0.))
 
     x = tensor.matrix('edges', dtype='int64')
-    y = tensor.vector('nodes', dtype='int64')
+    y = tensor.matrix('nodes', dtype='int64')
 
     n_steps = x.shape[0]
     n_samples = x.shape[1]
@@ -39,8 +41,8 @@ def build_model(options):
     params['Nembs'] = theano.shared(Nembs, name='Nembs')
     params['Eembs'] = theano.shared(Eembs, name='Eembs')
 
-    x_emb = params['Nembs'][x.flatten()].reshape(n_steps, n_samples, options['dim'])
-    y_emb = params['Eembs'[y.flatten()]].reshape(n_steps, n_samples, options['dim'])
+    x_emb = params['Nembs'][x.flatten()].reshape([n_steps, n_samples, options['dim']])
+    y_emb = params['Eembs'][y.flatten()].reshape([n_steps, n_samples, options['dim']])
 
     # build edge-to-nodes LSTM
     e2n_lstm = LSTM(options['dim'], prefix='e2n_lstm')
@@ -58,8 +60,147 @@ def build_model(options):
     xe_proj = n2e_lstm.output(xn_proj)
     xe_proj = xe_proj[1:]
 
-    ne_err = ((xn_proj - y_emb) ** 2).sum()
-    ee_err = ((xe_proj- x_emb[1:-1]) **2).sum()
+    ne_err = tensor.mean((xn_proj - y_emb) ** 2)
+    ee_err = tensor.mean((xe_proj- x_emb[1:-1]) **2)
+
+    cost = ne_err + ee_err
+
+    return use_noise, x, y, params, cost
+
+def get_minibatches_idx(n, minibatch_size, shuffle=False):
+    """
+    Used to shuffle the dataset at each iteration.
+    """
+
+    idx_list = numpy.arange(n, dtype="int32")
+
+    if shuffle:
+        numpy.random.shuffle(idx_list)
+
+    minibatches = []
+    minibatch_start = 0
+    for i in range(n // minibatch_size):
+        minibatches.append(idx_list[minibatch_start:
+                                    minibatch_start + minibatch_size])
+        minibatch_start += minibatch_size
+
+    if (minibatch_start != n):
+        # Make a minibatch out of what is left
+        minibatches.append(idx_list[minibatch_start:])
+
+    return zip(range(len(minibatches)), minibatches)
+
+def get_dataset():
+    node_corpus, edge_corpus, n_nodes, n_edges = cPickle.load(open('./citeseer/citeseer.corpus.pkl', 'rb'))
+    return node_corpus, edge_corpus, n_nodes, n_edges
+    pass
+
+def prepare_data(n_seqs, e_seqs, maxlen=None):
+    """Create the matrices from the datasets.
+
+    This pad each sequence to the same lenght: the lenght of the
+    longuest sequence or maxlen.
+
+    if maxlen is set, we will cut all sequence to this maximum
+    lenght.
+
+    This swap the axis!
+    """
+    # x: a list of sentences
+    lengths = [len(s) for s in n_seqs]
+
+    if maxlen is not None:
+        new_n_seqs = []
+        new_e_seqs = []
+        new_lengths = []
+        for l, n, e in zip(lengths, n_seqs, e_seqs):
+            if l < maxlen:
+                new_n_seqs.append(n)
+                new_e_seqs.append(e)
+                new_lengths.append(l)
+
+        lengths = new_lengths
+        e_seqs = new_e_seqs
+        n_seqs = new_n_seqs
+
+        if len(lengths) < 1:
+            return None, None, None
+
+    n_samples = len(n_seqs)
+    maxlen = numpy.max(lengths)
+
+    x = numpy.zeros((maxlen, n_samples)).astype('int64')
+    y = numpy.zeros((maxlen, n_samples)).astype('int64')
+
+    for idx, s in enumerate(n_seqs):
+        x[:lengths[idx], idx] = s
+    for idx, e in enumerate(e_seqs):
+        x[:lengths[idx]-1, idx] = e
+
+    return x, y
+
+def train(
+        dim = 128,
+        batch_size=64,
+        use_dropout=True,
+        max_epochs = 1,
+        lrate=0.0001,
+        dispFreq = 10,
+    ):
+    options = locals().copy()
+    print 'Model Options:', options
+
+    node_corpus, edge_corpus, n_nodes, n_edges = get_dataset()
+    options['n_nodes'] = n_nodes
+    options['n_edges'] = n_edges
+
+    print 'Building Model'
+    use_noise, x, y, params, cost = build_model(options)
+
+    f_cost = theano.function([x, y], cost, name='f_cost')
+
+    grads = tensor.grad(cost, wrt=list(params.values()))
+    f_grad = theano.function([x, y], grads, name='f_grad')
+
+    lr = tensor.scalar(name='lr')
+    f_grad_shared, f_update = adadelta(lr, params, grads,
+                                        [x, y], cost)
+    uidx = 0  # the number of update done
+    estop = False  # early stop
+    start_time = time.time()
+    try:
+        for eidx in range(max_epochs):
+            n_samples = 0
+
+            # Get new shuffled index for the training set.
+            kf = get_minibatches_idx(len(node_corpus), batch_size, shuffle=True)
+
+            for _, train_index in kf:
+                uidx += 1
+                use_noise.set_value(1.)
+
+                # Select the random examples for this minibatch
+                y = [node_corpus[t] for t in train_index]
+                x = [edge_corpus[t] for t in train_index]
+
+                x, y = prepare_data(x, y)
+                # Get the data in numpy.ndarray format
+                # This swap the axis!
+                # Return something of shape (minibatch maxlen, n samples)
+                n_samples += x.shape[1]
+
+                cost = f_grad_shared(x, y)
+                f_update(lrate)
+
+                if numpy.mod(uidx, dispFreq) == 0:
+                    print('Epoch ', eidx, 'Update ', uidx, 'Cost ', cost)
+
+    except KeyboardInterrupt:
+        print("Training interupted")
+
+if __name__ == '__main__':
+    train()
+
 
 
 
--- scripts/walk-edges.py
+++ scripts/walk-edges.py
@@ -9,14 +9,18 @@ def add_edge(graph, x, y, num):
 def load_graph(path, undirected=True):
     fr = open(path, 'rb')
     graph = defaultdict()
+    n_nodes = 0
+    n_edges = 0
     for no, line in enumerate(fr):
         x, y = line.strip().split()[:2]
         x = int(x)
         y = int(y)
         add_edge(graph, x, y, no)
+        n_edges += 1
         if undirected:
             add_edge(graph, y, x, no)
-    return graph
+    n_nodes = len(graph.keys())
+    return graph, n_nodes, n_edges
 
 def scan(walks_path, graph):
     node_corpus = []
@@ -34,12 +38,16 @@ def scan(walks_path, graph):
         assert len(edges_seq) + 1 == len(nodes_seq)
         node_corpus.append(nodes_seq)
         edge_corpus.append(edges_seq)
-    print len(node_corpus), len(edge_corpus)
-    cPickle.dump((node_corpus, edge_corpus), open('../citeseer/citeseer.corpus.pkl', 'wb'))
-if __name__ == '__main__':
-    graph = load_graph('../citeseer/graph.txt')
-    scan('../citeseer/citeseer.walks.txt', graph)
 
+    return node_corpus, edge_corpus
+
+
+if __name__ == '__main__':
+    graph, n_nodes, n_edges = load_graph('../citeseer/graph.txt')
+    print n_nodes, n_edges
+    node_corpus,edge_corpus = scan('../citeseer/citeseer.walks.txt', graph)
+    print len(node_corpus), len(edge_corpus)
+    cPickle.dump((node_corpus, edge_corpus, n_nodes, n_edges), open('../citeseer/citeseer.corpus.pkl', 'wb'))
 
 
 
